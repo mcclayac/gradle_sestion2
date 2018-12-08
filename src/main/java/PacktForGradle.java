944e4beca31fd306e9f03da7b6e1c38ae1ec5bf4
mcclayac <anthony.mcclay@me.com>
2018-12-08 09:02:23 -0600

    Added Macbook Pro println

--- src/main/java/PacktForGradle.java
+++ src/main/java/PacktForGradle.java
@@ -4,5 +4,6 @@ public class PacktForGradle  {
 
     public static void main(String[] args) {
         System.out.println("Hello World - These nuts #2 - from Windos 10");
+        System.out.println("Hello World - These nuts #2 - from Mac Book Pro");
     }
 }
