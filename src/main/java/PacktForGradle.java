1aa86c5806af6e7ec4eb7268f3608de160b6f903
mcclayac <anthony.mcclay@me.com>
2018-12-09 04:33:50 -0600

    Gradle wrapper and changes to the one source code and .getignore

--- src/main/java/PacktForGradle.java
+++ src/main/java/PacktForGradle.java
@@ -4,6 +4,9 @@ public class PacktForGradle  {
 
     public static void main(String[] args) {
         System.out.println("Hello World - These nuts #2 - from Windos 10");
+        System.out.println("Hello World - These nuts #2 - from Windos 10");
         System.out.println("Hello World - These nuts #2 - from Mac Book Pro");
+        System.out.println("Hello World - These nuts #2 - from Mac Book");
+        System.out.println("Hello World - These nuts #2 - from Mac Book");
     }
 }
